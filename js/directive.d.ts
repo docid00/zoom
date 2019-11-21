0c135fd2be38a13e36165a52ce732d58026ab535
Niels Plath <plath@schaffrath-digital.de>
2019-11-21 09:47:48 +0100

    [TASK] add type definition for proper usage in TS projects

---
+++ js/directive.d.ts
@@ -0,0 +1,18 @@
+declare interface zoomClassNames {
+    active? :string;
+    transition? :string;
+    visible? :string;
+}
+
+declare interface zoomSettings {
+    scaleDefault? :number;
+    scaleDifference? :number;
+    scaleMin? :number;
+    scaleMax? :number;
+    onZoomStart? :Function;
+    onZoomEnd? :Function;
+}
+
+declare function zoom(classNames: zoomClassNames, settings: zoomSettings): void
+
+export default zoom;
